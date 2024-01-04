cf6592ebdb30958442dab5ebd65e7b935d525630
Darkcheftar <darkcheftar.connect@gmail.com>
2024-01-04 12:05:19 +0530

    initial commit

---
+++ NoteApp/MainPageViewModel.cs
@@ -0,0 +1,51 @@
+﻿using NoteApp.Models;
+using System;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using Xamarin.Forms;
+
+namespace XAMLUI.ViewModels
+{
+    public class MainPageViewModel : INotifyPropertyChanged
+    {
+        public MainPageViewModel()
+        {
+            Notes = new ObservableCollection<NoteModel>();
+            DeleteNoteCommand = new Command<NoteModel>((NoteModel NoteToBeRemoved) =>
+            {
+                Notes.Remove(NoteToBeRemoved);
+            });
+            SaveNoteCommand = new Command(() =>
+            {
+                Notes.Add(new NoteModel { Text = NoteText });
+                NoteText = string.Empty;
+            },
+            () => !string.IsNullOrEmpty(NoteText));
+
+            EraseNotesCommand = new Command(() => Notes.Clear());
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        string noteText;
+        public string NoteText
+        {
+            get => noteText;
+            set
+            {
+                noteText = value;
+                PropertyChanged?.Invoke(this,
+                    new PropertyChangedEventArgs(nameof(NoteText)));
+
+                SaveNoteCommand.ChangeCanExecute();
+            }
+        }
+
+        public ObservableCollection<NoteModel> Notes { get; }
+
+        public Command SaveNoteCommand { get; }
+        public Command EraseNotesCommand { get; }
+        public Command DeleteNoteCommand { get; }
+    }
+}
---
+++ NoteApp/Models/NoteModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NoteApp.Models
+{
+    public class NoteModel
+    {
+        public string Text { get; set; }
+    }
+}
